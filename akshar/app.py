dc888da556b8ef470431af2a05ec9ad7379931db
shashank <shashank.f1@gmail.com>
2018-04-21 18:06:43 -0700

    bug fixes

--- akshar/app.py
+++ akshar/app.py
@@ -1,9 +1,11 @@
 import fire
+from QuillLanguage import QuillLanguage
+from get_data import data
 
 class App(object):
 
   def transform(self, language, word):
-    lang_object = qlang.QuillLanguage(data.language_definition[language], data.language_knowledge[language])
+    lang_object = QuillLanguage(data.language_definition[language], data.language_knowledge[language])
     result = lang_object.strToUnicode(word)
     for item in result:
         print(''.join(item))
